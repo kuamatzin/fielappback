1a4146931d882b4570710ed2223d2d6d97980518
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-03-24 12:30:12 -0600

    Business

---
+++ resources/assets/js/components/Example.vue
@@ -0,0 +1,23 @@
+<template>
+        <div class="panel panel-default">
+            <div class="panel-heading">Example Component</div>
+
+            <div class="panel-body">
+                I'm an example component!
+            </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+
+            axios.get('/api/user')
+                    .then(response => {
+                        console.log(response.data)
+                    })
+                    
+        }
+    }
+</script>
