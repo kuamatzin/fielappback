2feac1383706fd51d3927ddfb2afea6fdcfe10a4
Carlos Cuamatzin <kuamatzin@gmail.com>
2017-05-09 12:31:19 -0500

    Sellar tarjeta

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,31 @@ Route::get('/cards/{businesses_type}', function ($businesses_type) {
     return $cards;
 });
 
+Route::get('test', function(){
+    $cards = User::findOrFail(31)->cards()->where('completed', true)->get();
+    dd($cards);
+});
+
+Route::get('/signCard/{card_id}/{user_id}', function($card_id, $user_id){
+    //Existe un record con la tarjeta entonces hay que sellarla
+    $user = User::find($user_id);
+
+    if($card = $user->cards->find($card_id)){
+        $total_uses_to_complete = $card->uses;
+        $current_uses_plus_one = $card->pivot->uses + 1;
+        if (!$completed = $card->pivot->completed) {
+            $params_to_update = $total_uses_to_complete == $current_uses_plus_one ? ['uses' => $current_uses_plus_one, 'completed' => true] : ['uses' => $current_uses_plus_one];
+            $user->cards()->updateExistingPivot($card_id, $params_to_update, true);
+        }
+        else {
+            dd("Esta tarjeta ha sido completada");
+        }
+    }
+    else {
+        $user->cards()->attach($card_id, ['uses' => 1]);
+    }
+});
+
 Auth::routes();
 
 Route::get('/home', 'HomeController@index');
